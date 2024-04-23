3b80c014c9ef10d564a312fca542174fc96cd8b8
JEANCARLO TORRES FARRO <jean-12-t@hotmail.com>
2024-04-23 12:20:05 -0500

    crear el repository y las clases de servicios y el metodo show de producto controller

---
+++ src/main/java/com/ecomerce/repository/ProductoRepository.java
@@ -0,0 +1,10 @@
+package com.ecomerce.repository;
+
+import com.ecomerce.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+}
---
+++ src/main/java/com/ecomerce/service/ProductoServiceImplements.java
@@ -0,0 +1,36 @@
+package com.ecomerce.service;
+
+
+import com.ecomerce.model.Producto;
+import com.ecomerce.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductoServiceImplements implements ProductoService {
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public Producto save(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public Optional<Producto> get(Integer id) {
+        return productoRepository.findById(id);
+    }
+
+    @Override
+    public void update(Producto producto) {
+        productoRepository.save(producto);
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productoRepository.deleteById(id);
+    }
+}
