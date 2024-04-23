bbbc683ff47e267de0906816d3ea01079a62e062
JEANCARLO TORRES FARRO <jean-12-t@hotmail.com>
2024-04-23 13:43:20 -0500

    Vista create y logger para la vita usuario

--- src/main/java/com/ecomerce/controller/ProductoController.java
+++ src/main/java/com/ecomerce/controller/ProductoController.java
@@ -1,16 +1,41 @@
 package com.ecomerce.controller;
 
+import com.ecomerce.model.Producto;
+import com.ecomerce.model.Usuario;
+import com.ecomerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+    @Autowired
+    private ProductoService productoService;
+
     @GetMapping("")
     public String show() {
 
         return "/administrador/productos/show";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "/administrador/productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto) {
+        LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario usuario=new Usuario(1,"","","","","","","");
+        producto.setUsuario(usuario);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 
 }
