af48f92bdd80b07c9e68812e88a6c4cdda3b9f3b
JEANCARLO TORRES FARRO <jean-12-t@hotmail.com>
2024-04-26 10:13:43 -0500

    añadir los productos al carrito

--- src/main/java/com/ecomerce/controller/HomeController.java
+++ src/main/java/com/ecomerce/controller/HomeController.java
@@ -43,7 +43,7 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
         double sumaTotal=0;
@@ -51,6 +51,21 @@ public class HomeController {
         Optional<Producto> optionalProducto = productoService.get(id);
         log.info("Producto añadido: {}",optionalProducto.get());
         log.info("Cantidad: {}",cantidad);
+        producto=optionalProducto.get();
+
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(Double.parseDouble(producto.getPrecio()));
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(Double.parseDouble(producto.getPrecio())*cantidad);
+        detalleOrden.setProducto(producto);
+
+        detalles.add(detalleOrden);
+
+        sumaTotal=detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart",detalles);
+        model.addAttribute("orden",orden);
+
 
         return "usuario/carrito";
     }
