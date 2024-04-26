d63e25d7650e605b39b9bd91baacbc0e404f6b8a
JEANCARLO TORRES FARRO <jean-12-t@hotmail.com>
2024-04-26 13:17:08 -0500

    metodo para generar el numero de la orden

--- src/main/java/com/ecomerce/service/IOrdenService.java
+++ src/main/java/com/ecomerce/service/IOrdenService.java
@@ -1,7 +1,10 @@
 package com.ecomerce.service;
 import com.ecomerce.model.Orden;
 
+import java.util.List;
+
 
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save (Orden orden);
 }
