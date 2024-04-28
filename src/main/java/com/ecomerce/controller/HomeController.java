2dde744b83eb3052f4a0e76a858e1d8f7bf900a5
JEANCARLO TORRES FARRO <jean-12-t@hotmail.com>
2024-04-27 19:10:47 -0500

    implementar template diferente para usuario logeado

--- src/main/java/com/ecomerce/controller/HomeController.java
+++ src/main/java/com/ecomerce/controller/HomeController.java
@@ -44,6 +44,11 @@ public class HomeController {
     public String home(Model model, HttpSession session) {
         log.info("Session de usuario: {}",session.getAttribute("idusuario"));
         model.addAttribute("productos", productoService.findAll());
+
+        //session
+
+        model.addAttribute("session", session.getAttribute("idusuario"));
+
         return "usuario/home";
     }
 
@@ -113,10 +118,14 @@ public class HomeController {
     }
 
     @GetMapping("/getCart")
-    public String getCart(Model model) {
+    public String getCart(Model model, HttpSession session) {
 
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
+
+        // session
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+
         return "/usuario/carrito";
     }
 
